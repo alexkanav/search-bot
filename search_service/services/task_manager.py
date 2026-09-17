8b4cba9fdd1bf7f3e6781699cb3c667bdb2d144f
alex <sserinua@gmail.com>
2026-09-17 11:21:06 +0300

    fix: prevent stale task callback from removing current task

--- search_service/services/task_manager.py
+++ search_service/services/task_manager.py
@@ -9,9 +9,15 @@ class TaskManager:
 
     def start(self, chat_id: int, coro: Coroutine[Any, Any, Any]) -> asyncio.Task:
         self.cancel(chat_id)
+
         task = asyncio.create_task(coro)
         self._tasks[chat_id] = task
-        task.add_done_callback(lambda _: self._tasks.pop(chat_id, None))
+
+        def remove_task(done_task: asyncio.Task) -> None:
+            if self._tasks.get(chat_id) is done_task:
+                self._tasks.pop(chat_id, None)
+
+        task.add_done_callback(remove_task)
         return task
 
     def cancel(self, chat_id: int) -> None:
